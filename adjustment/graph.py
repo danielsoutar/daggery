8138ffb2d25e395a04dbbda1742ab001016e2db0
Daniel Soutar <danielsoutar144@gmail.com>
2024-12-01 22:47:07 +0000

    Add factory function for logger

--- adjustment/graph.py
+++ adjustment/graph.py
@@ -6,31 +6,39 @@ from pydantic import BaseModel
 
 from .node import Bar, Baz, Foo, Node, Quux, Qux
 
-# Create a logger instance
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
 
-# Create a console handler and set the level to INFO
-console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.INFO)
+def logger_factory() -> logging.Logger:
+    # Create a logger instance
+    logger = logging.getLogger(__name__)
 
-# Create a color formatter and set it for the handler
-formatter = colorlog.ColoredFormatter(
-    fmt="%(log_color)s%(levelname)s%(reset)s: %(asctime)s [%(name)s]  %(message)s",
-    datefmt=None,
-    reset=True,
-    log_colors={
-        "DEBUG": "cyan",
-        "INFO": "green",
-        "WARNING": "yellow",
-        "ERROR": "red",
-        "CRITICAL": "red,bg_white",
-    },
-)
-console_handler.setFormatter(formatter)
+    logger.setLevel(logging.INFO)
 
-# Add the handler to the logger
-logger.addHandler(console_handler)
+    # Create a console handler and set the level to INFO
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+
+    # Create a color formatter and set it for the handler
+    formatter = colorlog.ColoredFormatter(
+        fmt="%(log_color)s%(levelname)s%(reset)s: %(asctime)s [%(name)s]  %(message)s",
+        datefmt=None,
+        reset=True,
+        log_colors={
+            "DEBUG": "cyan",
+            "INFO": "green",
+            "WARNING": "yellow",
+            "ERROR": "red",
+            "CRITICAL": "red,bg_white",
+        },
+    )
+    console_handler.setFormatter(formatter)
+
+    # Add the handler to the logger
+    logger.addHandler(console_handler)
+
+    return logger
+
+
+logger = logger_factory()
 
 
 class DAG(BaseModel):
